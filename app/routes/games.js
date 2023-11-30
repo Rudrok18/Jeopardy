211aa197d543b611b2266d913309ef42459529d0
rolopezc03 <rodrigo.lopez@iteso.mx>
2023-11-30 16:18:56 -0600

    First GETs implementations

--- app/routes/games.js
+++ app/routes/games.js
@@ -6,17 +6,30 @@ const dataHandler = require('./../controllers/data_handler');
 
 router.route('/')
     .get((req, res) => {
-        dataHandler.getGames();
-    })
+        let query = req.query.fitler;
 
-router.route('/games')
-        .post((req, res) => {
-            
-        })
+        let games;
+
+        if (query == undefined) {
+            try {
+                games = dataHandler.getGames();
+            } catch(e) {
+                res.status(400).send("Error");
+            }
+            res.status(200).json(games);
+        }
+    })
 
 router.route('/:id')
     .get((req, res) => {
+        let uuid = req.params.id;
+        let game = dataHandler.getGameById(uuid);
 
+        if (game != undefined) {
+            res.status(200).json(game)
+        } else {
+            res.status(400).send("ID not found")
+        }
     })
 
 module.exports = router;
